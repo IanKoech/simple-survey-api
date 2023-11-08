ea175b32e92dcdffccf13d6e9e1484246bd26c0d
IanKoech <iankoech9@gmail.com>
2023-11-08 14:51:51 +0300

    :construction: Added promise to ascertain db connection

--- config/db.js
+++ config/db.js
@@ -1,4 +1,13 @@
 const pgp = require('pg-promise')();
 const db = pgp('postgres://postgres:iankoech@localhost:5432/sky_survey_db');
 
+db.connect()
+    .then(obj => {
+        console.log('Database connection successful');
+        obj.done(); //releases the db connection
+    })
+    .catch(errror => {
+        console.log('Error connecting to the database :', error);
+    });
+    
 module.exports = db;
