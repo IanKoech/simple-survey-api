9cb25d00892d2e2cf452801893b980022713230b
IanKoech <iankoech9@gmail.com>
2023-11-08 13:54:35 +0300

    :construction: Created endpoints for fetching and creating survey

---
+++ app/controllers/surveyController.js
@@ -0,0 +1,26 @@
+const db = require('../config/db');
+
+const surveyController = {
+  getAllSurveys: async (req, res) => {
+    try {
+      const surveys = await db.any('SELECT * FROM survey');
+      res.json(surveys);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  },
+
+  createSurvey: async (req, res) => {
+    const { name, description } = req.body;
+    try {
+      await db.none('INSERT INTO survey(name, description) VALUES($1, $2)', [name, description]);
+      res.json({ message: 'Survey created' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+};
+
+module.exports = surveyController;
