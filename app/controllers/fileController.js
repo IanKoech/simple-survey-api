041a9fd90233733429f46e665c2323c3ed9f07bb
IanKoech <iankoech9@gmail.com>
2023-11-10 09:34:59 +0300

    :construction: Completed controllers for all entities

--- app/controllers/fileController.js
+++ app/controllers/fileController.js
@@ -29,6 +29,37 @@ const fileController = {
             console.error(error);
             res.status(500).json({ message: 'Internal Server Error' });
         }
+    },
+    updateFile: async (req, res) => {
+        const fileId = req.params.id;
+        const { response_id, file_name, format, file_size } = req.body;
+        try {
+            await db.none(
+                "UPDATE file SET response_id=$1, file_name=$2, format=$3, file_size=$4 WHERE file_id=$5",
+                [
+                    response_id,
+                    file_name,
+                    format,
+                    file_size,
+                    fileId
+                ]
+            );
+            res.json({ message: "File update successful" });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    },
+    
+    deleteFile: async (req, res) => {
+        const fileId = req.params.id;
+        try {
+            await db.none("DELETE FROM file WHERE file_id = $1", [fileId]);
+            res.json({ message: 'File deleted' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     }
 };
 
--- app/controllers/responseDetailController.js
+++ app/controllers/responseDetailController.js
@@ -28,7 +28,34 @@ const responseDetailController = {
             console.error(error);
             res.status(500).json({ message: 'Internal Server Error' });
         }
-    }
+    },
+
+    updateResponseDetail: async (req, res) => {
+        const responseDetailId = req.params.id;
+        const { question_id, response_id, response } = req.body;
+        try {
+          await db.none(
+            "UPDATE response_detail SET question_id=$1, response_id=$2, response=$3 WHERE response_detail_id=$4",
+            [question_id, response_id, response, responseDetailId]
+          );
+          res.json({ message: "Response Detail updated" });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: "Internal Server Error" });
+        }
+      },
+    
+      deleteResponseDetail: async (req, res) => {
+        const responseDetailId = req.params.id;
+        try {
+          await db.none("DELETE FROM response_detail WHERE response_detail_id = $1", [responseDetailId]);
+          res.json({ message: "Response Detail deleted" });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: "Internal Server Error" });
+        }
+      }
+
 };
 
 module.exports = responseDetailController;
